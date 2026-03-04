04bfcfdb7ff736ddb496432967ee01183807a2c9
Anna <socevilaf@gmail.com>
2026-03-04 15:10:38 +0300

    Create baseApi

---
+++ src/shared/constants/api.ts
@@ -0,0 +1,5 @@
+export const API_BASE_URL = 'https://api.yeatwork.ru/api' as const;
+
+export const API_TAGS = {
+  Questions: 'Questions',
+} as const;
--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,8 @@ export default defineConfig({
       '@shared': path.resolve(__dirname, './src/shared'),
     },
   },
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
 });
