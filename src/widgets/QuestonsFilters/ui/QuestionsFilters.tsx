49703fc71dd252872972c818b990af399c0cef82
Anna <socevilaf@gmail.com>
2026-03-10 16:07:25 +0300

    Add styles

--- src/widgets/QuestonsFilters/ui/QuestionsFilters.tsx
+++ src/widgets/QuestonsFilters/ui/QuestionsFilters.tsx
@@ -1,9 +1,11 @@
 import { QuestionSearch } from '@/features/questions/questionsFilters';
 import { Card } from '@/shared/ui/Card';
 
+import classes from './QuestionsFilters.module.scss';
+
 const QuestionsFilters = () => {
   return (
-    <Card size="small">
+    <Card size="small" className={classes.filtersContainer}>
       <QuestionSearch />
     </Card>
   );
