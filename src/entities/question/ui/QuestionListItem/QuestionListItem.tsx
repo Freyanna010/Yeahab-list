a1e46273cb66189c6f293a16d7453100798a6c2e
Anna <socevilaf@gmail.com>
2026-03-26 19:06:27 +0300

    Fix QuestionListItem: delete li

--- src/entities/question/ui/QuestionListItem/QuestionListItem.tsx
+++ src/entities/question/ui/QuestionListItem/QuestionListItem.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import clsx from 'clsx';
 
 import { Accordion } from '@/shared/ui/Accordion';
 import { Title } from '@/shared/ui/Titel';
@@ -14,21 +13,18 @@ interface Props {
 
 const QuestionListItem = React.memo(function QuestionListItem({
   question,
-  className,
 }: Props) {
   return (
-    <li className={clsx(classes.root, className)} key={question.id}>
-      <Accordion
-        className={classes.acc}
-        title={
-          <Title level="h2" className={classes.title}>
-            {question.title}
-          </Title>
-        }
-      >
-        <QuestionBody question={question} />
-      </Accordion>
-    </li>
+    <Accordion
+      className={classes.acc}
+      title={
+        <Title level="h2" className={classes.title}>
+          {question.title}
+        </Title>
+      }
+    >
+      <QuestionBody question={question} />
+    </Accordion>
   );
 });
 
